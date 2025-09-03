90d761a133d42dd19fca6a9983b120d0423414dd
Caleb Staub <cstaub138@gmail.com>
2025-09-03 15:25:03 +0000

    Found and output BMI

--- main.cpp
+++ main.cpp
@@ -2,7 +2,19 @@
 using namespace std;
 
 int main() {
- 
+ int height_inches;
+ int weight_pounds;
+ double bmi;
+ double meters;
+ double kg;
 
+ height_inches = 61;
+ weight_pounds = 130;
+ meters = height_inches * 0.0254;
+ kg = weight_pounds * 0.453592;
+ bmi = kg / (meters * meters);
 
+ cout << "The BMI for this person is: "<< bmi << "." << endl;
+
+ return 0;
 }
